702fb838ba96b27ea8a6a517bb90abb4c5c0843b
joshua Takyi <takyijoshua019@gmail.com>
2025-03-12 16:13:41 +0000

    removed the readme file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	darkMode: "class",
-	content: ["./src/*html"],
+	content: [
+		"./index.html", // Root index.html
+		"./src/**/*.{html,js}", // All HTML/JS files in src folder
+	],
 	theme: {
 		extend: {
 			fontFamily: {
-				roboto: ["Roboto", "sans - serif"],
+				roboto: ["Roboto", "sans-serif"], // Fixed typo
 			},
 			colors: {
 				skin: {
 					head: "var(--color-text-head)",
 					text: "var(--color-text-para)",
-					background: "var( --color-background)",
+					background: "var(--color-background)",
 					icons: "var(--color-text-icons)",
 					bordercol: "var(--color-border)",
 					extext: "var(--color-text-ex)",
@@ -20,7 +23,7 @@ module.exports = {
 			backgroundColor: {
 				head: {
 					fade: "var(--color-background)",
-					buttonbg: "var( --color-text-head)",
+					buttonbg: "var(--color-text-head)",
 					hoverbg: "var(--color-hover-bg)",
 				},
 			},
